1be4fcc355821bbcade9a5eccac77ee4f941c295
Ronen Margolin <roni948576@gmail.com>
2022-12-14 21:08:12 +0200

    code will display all files in videos folder

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import cv2
 import logging
 from process import process
+import os
 
-cap = cv2.VideoCapture("videos/0001-5400.mp4")
+vid_files = os.listdir("videos")
 
-if not cap.isOpened():
-    logging.error("cant open file %s","videos/0001-5400.mp4")
-    exit(1)
+for f_vid in vid_files:
+    f_vid = "videos/"+f_vid
+    # open video
+    cap = cv2.VideoCapture(f_vid)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret == True:
+
+    if not cap.isOpened():
+        logging.error("cant open file %s",f_vid)
+        exit(1)
+    else:
+        logging.info("working on %s", f_vid)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            
+            frame = process(frame)
+
+            # Display the resulting frame
+            cv2.imshow('Frame',frame)
         
-        frame = process(frame)
-
-        # Display the resulting frame
-        cv2.imshow('Frame',frame)
-    
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        
+            # Break the loop
+        else: 
             break
-    
-        # Break the loop
-    else: 
-        break
